5d3024f843839e21aa5c95166d40d06bea1daa22
autinn123 <autinn123@gmail.com>
2021-06-10 16:31:38 +0700

    Sign In

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import SignIn from './pages/Usecase_16/SignIn';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/sign-in" exact component={SignIn}></Route>
+      </Switch>
+    </Router>
   );
 }
 
